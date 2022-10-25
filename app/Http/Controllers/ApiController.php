efa66a87b5185b9e71a77f844b82f353a77276c0
aligilani5 <airtaza753@gmail.com>
2022-10-25 15:38:04 +0500

    top 5 categories api & change categories with pro

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,12 +8,25 @@ use App\Models\Product;
 
 class ApiController extends Controller
 {
-    //
+    //Queries categories with products having product price greater than 500 
     public function getAllCategoriesWithProductsPrice() {
+
         $categoriesWithProductsPrice = Category::whereHas('products', function($q){
+
+            return $q->where('price', '>', 500);
+        })->with('products', function($q){
+
             return $q->where('price', '>', 500);
         })->get();
 
         return $categoriesWithProductsPrice;
+    }
+
+    //Queries top 5 categories with products that are new listed
+    public function getCategoriesWithNewListedProducts() {
+
+        $newListedProducts = Category::with('products')->latest()->limit(5)->get();
+
+        return $newListedProducts;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('getAllCategoriesWithProductsPrice', [ApiController::class, 'getAllCategoriesWithProductsPrice']);
+Route::get('getCategoriesWithNewListedProducts', [ApiController::class, 'getCategoriesWithNewListedProducts']);
